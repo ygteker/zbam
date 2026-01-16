52e45983df5c8559c64cbe5b13fc382c623b1f16
ygteker <gunes.teker@gmail.com>
2026-01-16 23:35:23 +0100

    Initial Commit

---
+++ Zbam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zbam
+//
+//  Created by Yagiz Gunes Teker on 16.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zbam/ZbamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZbamApp.swift
+//  Zbam
+//
+//  Created by Yagiz Gunes Teker on 16.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ZbamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
