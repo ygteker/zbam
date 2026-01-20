d117be94bd320095bfc852fc3d07b190589fc1f5
ygteker <gunes.teker@gmail.com>
2026-01-20 17:44:01 +0100

    experimental
    (cherry picked from commit 2dcaec4251cd0c3e6a8ed6adb5f4f33486b027b6)

--- Zbam/Views/CardView.swift
+++ Zbam/Views/CardView.swift
@@ -9,79 +9,44 @@ import SwiftUI
 import SwiftData
 
 struct CardView: View {
+    enum SwipeDirection {
+        case left, right, none
+    }
+    
+    struct Model: Identifiable, Equatable {
+        let id: UUID = UUID()
+        let text: String
+        var swipeDirection: SwipeDirection = .none
+    }
+    
+    var model: Model
+    var size: CGSize
+    var dragOffset: CGSize
+    var isTopCard: Bool
+    var isSecondCard: Bool
+    
     let card: Card
     @State private var isPresentingEditView: Bool = false
     @State private var flipped: Bool = false
     
-    init(card: Card) {
-        self.card = card
-    }
-    
     var body: some View {
-        let flipDegrees = flipped ? 180.0 : 0
-        VStack {
-            NavigationStack {
-                VStack {
-                    Spacer()
-                    ZStack {
-                        Text(card.front)
-                            .placedOnCard(Color.yellow)
-                            .flipRotate(flipDegrees)
-                            .opacity(flipped ? 0.0 : 1.0)
-                        Text(card.back)
-                            .placedOnCard(Color.blue)
-                            .flipRotate(-180 + flipDegrees)
-                            .opacity(flipped ? 1.0 : 0.0)
-                    }
-                    // Add margins around the card area
-                    .padding(.horizontal, 64)
-                    .padding(.vertical, 56)
-                    .animation(.easeInOut(duration: 0.8), value: flipped)
-                    .onTapGesture {
-                        flipped.toggle()
-                    }
-                    Spacer()
-                }
-                .toolbar {
-                    Button("Edit") {
-                        isPresentingEditView = true
-                    }
-                }
-                .sheet(isPresented: $isPresentingEditView) {
-                    NavigationStack {
-                        CardEditView(card: card)
-                            .navigationTitle("Card")
-                    }
-                }
-            }
-        }
-    }
-}
-
-extension View {
-    func flipRotate(_ degrees : Double) -> some View {
-        return rotation3DEffect(Angle(degrees: degrees), axis: (x: 0.0, y: 1.0, z: 0.0))
+        Text(model.text)
+            .frame(width: size.width * 0.8, height: size.height * 0.8)
+            .background(Color.white)
+            .cornerRadius(12.0)
+            .shadow(color: isTopCard ? getShadowColor() : (isSecondCard && dragOffset.width != 0 ? Color.gray.opacity(0.2) : Color.clear), radius: 10, x: 0, y: 3)
+            .foregroundColor(.black)
+            .font(.largeTitle)
+            .padding()
     }
     
-    func placedOnCard(_ color: Color) -> some View {
-        return padding(5)
-            .fontDesign(.serif)
-            .font(.system(size: 54))
-            .lineLimit(2)
-            .minimumScaleFactor(0.4)
-            .frame(maxWidth: 600, maxHeight: 600)
-            .background(color)
-            .cornerRadius(12.0)
-            .shadow(radius: 12)
-    }
-}
-
-#Preview(Card.sampleData.first?.front ?? "Card Preview", traits: .cardSampleData) {
-    // Use the first sample card
-    if let first = Card.sampleData.first {
-        CardView(card: first)
-    } else {
-        // Fallback if sample data is empty
-        CardView(card: Card(front: "Frontjasdasdsadssdasdasdasddasdsad", back: "Back"))
+    private func getShadowColor() -> Color {
+        if dragOffset.width > 0 {
+            return Color.green.opacity(0.5)
+        } else if dragOffset.width < 0 {
+            return Color.red.opacity(0.5)
+        } else {
+            return Color.gray.opacity(0.2)
+        }
     }
 }
